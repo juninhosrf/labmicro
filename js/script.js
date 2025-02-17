ef576f641d0de4c5427a3f6d0f7f242046e0b23d
Willians Junior <willianscpjr@gmail.com>
2025-02-17 10:22:09 -0300

    fix pages

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 function loadComponent(id, file) {
+    let basePath = window.location.hostname.includes("github.io") ? "/labmicro/" : "/";
+
     let pathPrefix = window.location.pathname.includes("/pages/") ? "../" : "";
+    
+    // Se estiver no GitHub Pages, corrige o caminho absoluto
+    if (window.location.hostname.includes("github.io")) {
+        pathPrefix = basePath;
+    }
 
     fetch(pathPrefix + file)
         .then(response => {
@@ -18,3 +25,4 @@ document.addEventListener("DOMContentLoaded", function () {
     loadComponent("navbar", "pages/navbar.html");
     loadComponent("footer", "pages/footer.html");
 });
+
